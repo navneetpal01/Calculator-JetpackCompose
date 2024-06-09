9b74b0dd3f7052fd51b56ccad6e0ac74621d7ddf
navneet <navneetpalgithub@gmail.com>
2024-06-09 22:13:57 +0530

    Mars

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.example.calculator.screen.home.HomeScreen
 import com.example.calculator.ui.theme.CalculatorTheme
 
 
