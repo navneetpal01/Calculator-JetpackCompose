94e8b4524486517808eaad50c9f8c203b0997569
navneet <navneetpalgithub@gmail.com>
2024-06-09 22:12:49 +0530

    Mars

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -6,6 +6,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import com.example.calculator.ui.theme.CalculatorTheme
 
 
@@ -19,11 +23,15 @@ class MainActivity : ComponentActivity() {
         )
         super.onCreate(savedInstanceState)
         setContent {
-            CalculatorTheme {
-
+            CalculatorTheme(darkTheme = true) {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreen()
+                }
             }
         }
     }
-
-
 }
---
+++ app/src/main/java/com/example/calculator/screen/home/HomeScreen.kt
@@ -0,0 +1,12 @@
+package com.example.calculator.screen.home
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.runtime.Composable
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(){
+    val scaffoldState = rememberBottomSheetScaffoldState()
+}
