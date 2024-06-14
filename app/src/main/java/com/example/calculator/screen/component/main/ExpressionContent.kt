95436436fb559ed3e86dbe42b91ee91f89fa3d29
navneet <navneetpalgithub@gmail.com>
2024-06-14 19:27:35 +0530

    Mars

--- app/src/main/java/com/example/calculator/screen/component/main/ExpressionContent.kt
+++ app/src/main/java/com/example/calculator/screen/component/main/ExpressionContent.kt
@@ -1,8 +1,17 @@
 package com.example.calculator.screen.component.main
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -14,6 +23,21 @@ fun ExpressionContent(
     updateTextFieldValue : (TextFieldValue) -> Unit
 ){
 
+    val scrollstate = rememberScrollState()
 
+    val customTextSelectionColors = TextSelectionColors(
+        handleColor = Color.Transparent,
+        backgroundColor = Color.LightGray
+    )
 
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(EXPRESSION_RESULT_HEIGHT),
+        contentAlignment = Alignment.Center
+    ){
+
+    }
 }
+
+val EXPRESSION_RESULT_HEIGHT = 128.dp
